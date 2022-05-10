4ade46686a7b5456adfa8d3145b95e456bc0fae8
Allan Gabriel <43339727+allangbr@users.noreply.github.com>
2022-05-10 11:13:40 -0300

    Implementando Inserts Tabela Projeto #1

--- tarefas/t01/tarefa01-inserts.sql
+++ tarefas/t01/tarefa01-inserts.sql
@@ -18,3 +18,15 @@ UPDATE funcionario SET cod_depto = 2 WHERE codigo = 2;
 UPDATE funcionario SET cod_depto = 3 WHERE codigo = 3;
 UPDATE funcionario SET cod_depto = 4 WHERE codigo = 4;
 UPDATE funcionario SET cod_depto = 5 WHERE codigo = 5;
+
+# Inserts Tabela Projeto
+INSERT INTO projeto (codigo, nome, descricao, cod_depto, cod_responsavel, data_inicio, data_fim) 
+VALUES (1, 'Análise de Sistemas', 'Projeto para ensinar como fazer a análise de sistemas de software', 1, 1, '2022/05/01', '2022/05/08')
+INSERT INTO projeto (codigo, nome, descricao, cod_depto, cod_responsavel, data_inicio, data_fim) 
+VALUES (2, 'Gerenciamento de Sistemas', 'Projeto para ensinar como fazer a gerencia de sistemas de software', 2, 2, '2022/06/02', '2022/06/09')
+INSERT INTO projeto (codigo, nome, descricao, cod_depto, cod_responsavel, data_inicio, data_fim) 
+VALUES (3, 'Gerenciamento de Dados de Sistemas', 'Projeto para ensinar como fazer a gerencia de dados guardados pelo software', 3, 3, '2022/07/03', '2022/07/10')
+INSERT INTO projeto (codigo, nome, descricao, cod_depto, cod_responsavel, data_inicio, data_fim) 
+VALUES (4, 'Programando Sistemas de Software', 'Projeto para ensinar como fazer o desenvolvimento de um software', 4, 4, '2022/08/04', '2022/08/11')
+INSERT INTO projeto (codigo, nome, descricao, cod_depto, cod_responsavel, data_inicio, data_fim) 
+VALUES (5, 'Gerenciando os Processos de Software', 'Projeto para ensinar como fazer o gerenciamento dos processos de desenvolvimento de um software', 5, 5, '2022/09/05', '2022/09/12')
