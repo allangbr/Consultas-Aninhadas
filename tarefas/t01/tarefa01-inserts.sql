d34a0e3a8bece4f2b316f44872b7b9f60c2fa712
Allan Gabriel <43339727+allangbr@users.noreply.github.com>
2022-05-10 10:53:52 -0300

    Implementando Inserts Table Funcionário #1

--- tarefas/t01/tarefa01-inserts.sql
+++ tarefas/t01/tarefa01-inserts.sql
@@ -1,1 +1,6 @@
-
+#Inserts Table Funcionário
+INSERT INTO funcionario (codigo, nome, sexo, dt_nasc, salario) VALUES (1, 'Allan', 'M', '2000/12/31', 10000.00)
+INSERT INTO funcionario (codigo, nome, sexo, dt_nasc, salario) VALUES (2, 'Joan', 'M', '2000/12/11', 9000.00)
+INSERT INTO funcionario (codigo, nome, sexo, dt_nasc, salario) VALUES (3, 'Thaynara', 'F', '2000/12/03', 100000.00)
+INSERT INTO funcionario (codigo, nome, sexo, dt_nasc, salario) VALUES (4, 'David', 'M', '2000/03/05', 50000.00)
+INSERT INTO funcionario (codigo, nome, sexo, dt_nasc, salario) VALUES (5, 'Wesley', 'M', '2000/06/15', 8000.00)
