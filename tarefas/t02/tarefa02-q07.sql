b54aaa0592e57c3afb02864f17fc52d8108fda9b
Allan Gabriel <43339727+allangbr@users.noreply.github.com>
2022-05-24 13:38:45 -0300

    Update tarefa02-q07.sql

--- tarefas/t02/tarefa02-q07.sql
+++ tarefas/t02/tarefa02-q07.sql
@@ -2,7 +2,7 @@ CREATE TRIGGER add_passageiro AFTER
 INSERT ON cliente_voo
 FOR EACH ROW
 UPDATE voo v
-SET num_passageiros = num_passageiros + 1;
+SET num_passageiros = num_passageiros + 1
 WHERE v.codigo = NEW.codigo_voo;
 
 # Inserindo um cliente
