539198c15324551937368052559805895258e07b
Allan Gabriel <allanalmeidagbr@gmail.com>
2022-06-27 23:04:09 -0300

    Implementando letra a questão 6 #3

--- tarefas/t03/orm/index.js
+++ tarefas/t03/orm/index.js
@@ -8,4 +8,10 @@
   await Projeto.sync();
   await Atividade.sync();
 
+  const newAtividade = Atividade.create({
+    descricao: 'ES - Atividade 3',
+    codProjeto: 4,
+    dataInicio: '2021-09-30',
+    dataFim: '2021-10-30'
+  })
 })()
