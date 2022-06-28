66f689bafd9409a152439132850a19a442171600
Allan Gabriel <allanalmeidagbr@gmail.com>
2022-06-27 21:44:09 -0300

    Implementando tabela atividade ORM #3

---
+++ tarefas/t03/orm/atividade.js
@@ -0,0 +1,33 @@
+const { Sequelize, Model } = require('sequelize')
+const Projeto = require('./projeto')
+const sequelize = new Sequelize('postgres', 'postgres', 'postgres', {
+  dialect: 'postgres',
+  host: 'localhost'
+})
+
+class Atividade extends Model {}
+
+Atividade.init(
+  {
+    codigo: {
+      type: Sequelize.INTEGER,
+      autoIncrement: true,
+      allowNull: false,
+      primaryKey: true
+    },
+    descricao: Sequelize.STRING(250),
+    codProjeto: {
+      type: Sequelize.INTEGER,
+      references: {
+        model: Projeto,
+        key: 'codigo',
+        deferrable: Sequelize.Deferrable.INITIALLY_IMMEDIATE
+      }
+    },
+    dataInicio: Sequelize.DATEONLY,
+    dataFim: Sequelize.DATEONLY,
+  },
+  { sequelize }
+)
+
+module.exports = Atividade
