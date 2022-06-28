8d867063b044341fc329057cf1478d82cc72c442
Allan Gabriel <allanalmeidagbr@gmail.com>
2022-06-27 23:16:26 -0300

    Implementando letra b questão 6 #3

--- tarefas/t03/orm/index.js
+++ tarefas/t03/orm/index.js
@@ -3,10 +3,10 @@
   const Departamento = require('./departamento')
   const Projeto = require('./projeto')
   const Atividade = require('./atividade')
-  await Funcionario.sync();
-  await Departamento.sync();
-  await Projeto.sync();
-  await Atividade.sync();
+  await Funcionario.sync()
+  await Departamento.sync()
+  await Projeto.sync()
+  await Atividade.sync()
 
   const newAtividade = Atividade.create({
     descricao: 'ES - Atividade 3',
@@ -14,4 +14,13 @@
     dataInicio: '2021-09-30',
     dataFim: '2021-10-30'
   })
+
+  const attProjeto = Projeto.update(
+    { codResponsavel: 2 },
+    {
+      where: {
+        codResponsavel: 1
+      }
+    }
+  )
 })()
