e342fb51eda47d1ab9088d822f32306faa780b7e
Allan Gabriel <allanalmeidagbr@gmail.com>
2022-06-27 21:43:41 -0300

    Implementando Tabela departamento ORM #3

---
+++ tarefas/t03/orm/departamento.js
@@ -0,0 +1,32 @@
+const { Sequelize, Model } = require('sequelize')
+const sequelize = new Sequelize('postgres', 'postgres', 'postgres', {
+  dialect: 'postgres',
+  host: 'localhost'
+})
+const Funcionario = require('./funcionario')
+
+class Departamento extends Model {}
+
+Departamento.init(
+  {
+    codigo: {
+      type: Sequelize.INTEGER,
+      autoIncrement: true,
+      allowNull: false,
+      primaryKey: true
+    },
+    sigla: Sequelize.STRING(10),
+    descricao: Sequelize.STRING(50),
+    codGerente: {
+      type: Sequelize.INTEGER,
+      references: {
+        model: Funcionario,
+        key: 'codigo',
+        deferrable: Sequelize.Deferrable.INITIALLY_IMMEDIATE
+      }
+    }
+  },
+  { sequelize }
+)
+
+module.exports = Departamento
