4503c63c6655361592a0a37fa0a1e11939dfb597
Allan Gabriel <allanalmeidagbr@gmail.com>
2022-06-27 21:43:58 -0300

    Implementando tabela projeto ORM #3

---
+++ tarefas/t03/orm/projeto.js
@@ -0,0 +1,43 @@
+const { Sequelize, Model } = require('sequelize')
+const Funcionario = require('./funcionario')
+const Departamento = require('./departamento')
+const sequelize = new Sequelize('postgres', 'postgres', 'postgres', {
+  dialect: 'postgres',
+  host: 'localhost'
+})
+
+class Projeto extends Model {}
+
+Projeto.init(
+  {
+    codigo: {
+      type: Sequelize.INTEGER,
+      autoIncrement: true,
+      allowNull: false,
+      primaryKey: true
+    },
+    nome: Sequelize.STRING(50),
+    descricao: Sequelize.STRING(250),
+    codResponsavel: {
+      type: Sequelize.INTEGER,
+      references: {
+        model: Funcionario,
+        key: 'codigo',
+        deferrable: Sequelize.Deferrable.INITIALLY_IMMEDIATE
+      }
+    },
+    codDepto: {
+      type: Sequelize.INTEGER,
+      references: {
+        model: Departamento,
+        key: 'codigo',
+        deferrable: Sequelize.Deferrable.INITIALLY_IMMEDIATE
+      }
+    },
+    dataInicio: Sequelize.DATEONLY,
+    dataFim: Sequelize.DATEONLY
+  },
+  { sequelize }
+)
+
+module.exports = Projeto
